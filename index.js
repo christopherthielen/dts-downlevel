15b3d8c313cba47026f35676439f04a5c0323446
Chris Thielen <cthielen@netflix.com>
2020-05-28 14:45:41 -0700

    feat: support --semver cli arg for choosing what features to strip.  Add rule for stripping getters. also use yargs.

--- index.js
+++ index.js
@@ -1,22 +1,33 @@
 #! /usr/bin/env node
-var replaceInFile = require('replace-in-file');
-var globs = process.argv.slice(2);
-globs = globs.length ? globs : ["**/*.d.ts"];
+const replaceInFile = require('replace-in-file');
+const semver = require('semver');
+const yargs = require('yargs')
+    .option('semver', {
+      description: 'The semver typescript version being targeted',
+      default: '>=3.0.0',
+      required: true,
+    }).argv;
 
-var rules = require('./rules');
+const args = yargs._;
+const globs = args.length ? args : ["**/*.d.ts"];
+
+const rules = require('./rules')
+    .filter(rule => semver.satisfies(rule.introduced, yargs.semver));
 
 rules.forEach(rule => {
-  var cmd = {
+  const cmd = {
     from: rule.replace,
     to: rule.with,
     files: globs,
   };
 
-  var results = replaceInFile.sync(cmd);
+  const results = replaceInFile.sync(cmd);
 
   if (results.length) {
-    console.log("Rule: " + rule.doc);
-    results.forEach(result => console.log("<- Modified: " + result));
+    console.log(`dts-downlevel: ${rule.doc}`);
+    results.forEach(result => console.log("<- dts-downlevel modified: " + result));
+  } else {
+    console.log(`dts-downlevel: ${rule.doc} (no matches)`);
   }
 });
 
--- rules.js
+++ rules.js
@@ -1,26 +1,38 @@
 module.exports = [
 
   {
+    introduced: "2.0.0",
     doc: "Remove references to @types",
     replace: /^\/\/\/ <reference types=.*$/gm,
     with: ""
   },
 
   {
+    introduced: "2.0.0",
     doc: "Remove `readonly`",
     replace: /^(\s*)readonly /gm,
     with: "$1"
   },
 
   {
+    introduced: "2.0.0",
     doc: "Remove `abstract` from properties",
     replace: /^(\s+)abstract (\w+: .*;)/gm,
     with: "$1$2"
   },
+
   {
+    introduced: "2.0.0",
     doc: "Remove constant initializer values",
     replace: /(const \w+) = .*;$/gm,
     with: "$1;"
+  },
+
+  {
+    introduced: "3.6.0",
+    doc: "Remove getters",
+    replace: /(\s+)get (\S+)\(\)/gm,
+    with: "$1$2"
   },
 
 ];
